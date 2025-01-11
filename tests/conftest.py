8b0d26657b582ec85d85b89c4c6e08442a551d73
Oksana Gordeeva <gordeeva.oxana24@yandex.ru>
2025-01-11 20:43:28 +0500

    Second commit: change conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,9 @@ from curl import *
 @pytest.fixture(scope="function")
 def driver():
     options = FirefoxOptions()
-    options.binary_location = "C:/Program Files/Mozilla Firefox/firefox.exe"
+
     options.add_argument("--windows-size=1200,600")
-    service = Service("C:/WebDriver/bin/geckodriver.exe")
-    driver = webdriver.Firefox(options = options, service = service)
+    driver = webdriver.Firefox(options = options)
     driver.get(main_site)
     yield driver
     driver.quit()
